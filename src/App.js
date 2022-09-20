6e8ec3689296e63d27d8b1ec3a572882c16a8b15
Deepika-Engineer <dnarkodu@gmail.com>
2022-09-20 17:40:18 +0530

    strict mode removed

--- src/App.js
+++ src/App.js
@@ -6,12 +6,10 @@ import { stateOptions } from "./custom-select-menu/data"
 class App extends React.Component {
   render() {
     return (
-      <React.StrictMode>
       <div className="">
         <h3>Multi Select Drop Down</h3>
         <CustomSelect option={[...stateOptions]} id='value' label='label'/>   
       </div>
-      </React.StrictMode>
     );
   }
 }
