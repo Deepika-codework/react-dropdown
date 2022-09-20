ce8ed9e6542e0ad5822452f23d0e35f4c7ef23fd
Deepika-Engineer <dnarkodu@gmail.com>
2022-09-20 16:11:56 +0530

    strict mode and Prop vslidation added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from "react";
 import CustomSelect from "./custom-select-menu/custom-select";
 
+import { stateOptions } from "./custom-select-menu/data"
+
 class App extends React.Component {
- 
-
   render() {
-
     return (
+      <React.StrictMode>
       <div className="">
         <h3>Multi Select Drop Down</h3>
-        <CustomSelect/>   
+        <CustomSelect option={[...stateOptions]} id='value' label='label'/>   
       </div>
+      </React.StrictMode>
     );
   }
 }
--- src/custom-select-menu/custom-select.jsx
+++ src/custom-select-menu/custom-select.jsx
@@ -1,52 +1,70 @@
-import React,{useState} from "react";
+import React, { useState,useMemo } from "react";
 import "./index.css";
-import {stateOptions} from "./data"
+import PropTypes from 'prop-types'
 
 
-const CustomSelect = props => {
-const [expanded,setExpanded]=useState(false)
-const [selVal,setSelVal]=useState([])
-  
-const showCheckboxes=() =>{
-  if (!expanded) {
-    setExpanded(true);
-  } else {
-    setExpanded(false);
+const CustomSelect = ({option,id='value',label='label'}) => {
+  const [expanded, setExpanded] = useState(false)
+  const [selVal, setSelVal] = useState([])
+
+  const showCheckboxes = () => {
+    if (!expanded) {
+      setExpanded(true);
+    } else {
+      setExpanded(false);
+    }
   }
-}
 
-const evaluateCheckBox=(val)=>{
-  return selVal.includes(val)
-}
+  
+  const evaluateCheckBox = (val) => {
+    return selVal.includes(val)
+  }
+
+
+ const dropdownWithCheckBox = useMemo(()=>{
+    return (option.map(data => (
+      <div key={data.value} >
+        <input type="checkbox"
+          checked={evaluateCheckBox(data.label)}
+          onChange={(e) => {
+            e.target.checked ? selVal.push(data.label) : selVal.pop(data.label);
+            setSelVal([...selVal])
+          }
+          }
+        />
+        <span>{data.label}</span></div>)
+    ))
+  },[selVal])
 
   return (
-  <div>
-    <div style={{marginBottom:'14px'}}>
-    Selected Value:<b>{selVal.join(',')}</b>
+    <div>
+      <div style={{ marginBottom: '14px' }}>
+        Selected Value:<b>{selVal.join(',')}</b>
+      </div>
+      <div className="multiselect">
+        <div className="selectBox " onClick={() => showCheckboxes()}>
+          <select>
+            <option>Select an option</option>
+          </select>
+          <div className="overSelect"></div>
+        </div>
+        <div id="checkboxes" style={{ display: `${expanded ? 'block' : 'none'}`, maxHeight: '50vh', 'overflow':'scroll' }}>
+        <input type="checkbox"
+                checked={selVal.length===Object.keys(option).length}
+                onChange={(e) => {
+                 e.target.checked ? setSelVal([...option.map(data=>data.label)]): setSelVal([])
+                }
+                }
+              /><span>Select All</span>
+              {dropdownWithCheckBox}
+        </div>
+      </div>
     </div>
-    <div className="multiselect">
-    <div className="selectBox " onClick={()=>showCheckboxes()}>
-      <select>
-        <option>Select an option</option>
-      </select>
-      <div className="overSelect"></div>
-    </div>
-    <div id="checkboxes"  style={{display:`${expanded?'block':'none'}`}}>
-    {expanded && stateOptions.map(data=>(
-        <div key={data.value}>
-           <input  type="checkbox"
-           checked={evaluateCheckBox(data.label)}
-                            onClick={(e) => {
-                                e.target.checked?selVal.push(data.label):selVal.pop(data.label);
-                                setSelVal([...selVal])
-                                }
-                            }
-                        />
-      {data.label}</div>)
-    )}
-  </div>
-  </div>
-  </div>
   );
 };
+CustomSelect.propTypes ={
+  option:PropTypes.array.isRequired,
+  label:PropTypes.string,
+  id:PropTypes.string
+}
 export default CustomSelect;
